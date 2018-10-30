e1ab7b007934c87e9e02659a5fe9cdca9fd075b8
ankitavaghani <44086884+ankitavaghani@users.noreply.github.com>
2018-10-30 19:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,12 +19,11 @@ def profile(name):
 
 @app.route('/reverse_sentence', methods=['GET','POST'])
 def reverse_sentence_post():
-	'''if request.method == 'GET':
+	if request.method == 'GET':
 		return render_template('reverse_sentence.html')
 	elif request.method == 'POST':
 		name = 'ankita'
-		return render_template('reverse_sentence.html', result=str(name))'''
-	return render_template('reverse_sentence.html', result=str('ankita'))
+		return render_template('reverse_sentence.html', result=str('ankita'))
 
 @app.route('/add_numbers', methods=['GET','POST'])
 def add_numbers_post():
